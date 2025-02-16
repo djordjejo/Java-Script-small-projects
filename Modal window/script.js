544b9e8ab5f8012673b8af7d2ef7d8e09a08b65f
Djordje Jovanovic <dj.jovanovic6@gmail.com>
2025-02-16 17:30:47 +0100

    Modal window
    Made a small project, to practice JS DOM manipulation.

--- Modal window/script.js
+++ Modal window/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let closeButton = document.querySelector(".close-modal");
+let buttons = document.querySelectorAll(".show-modal");
+const closeModal = function (){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+const openModal = function ()
+{
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", openModal);
+})
+
+closeButton.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal)
+
+document.addEventListener("keydown", (event)=>{
+    console.log(event)
+    if(event.key === "Escape")
+        if(!modal.classList.contains("hidden"))closeModal();
+
+        
+})
